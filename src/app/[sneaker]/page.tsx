862defd7bdf396cbb5d45fc125e57da7482389c2
runningfok <adrianfok26@gmail.com>
2023-08-14 15:15:54 +0800

    made chagne to [sneaker]

--- src/app/[sneaker]/page.tsx
+++ src/app/[sneaker]/page.tsx
@@ -15,7 +15,9 @@ export default async function SneakerPage({ params }: SneakerPageProps) {
 
   return (
     <div className="bg-[#FEFEFE] text-green-950">
-      <div className="max-w-fit 2xl:max-w-screen-2xl 2xl:px-16 mx-auto place-self-center p-10 2xl:py-36">
+      <div className="sm:max-w-fit 2xl:max-w-screen-2xl 2xl:px-16 mx-auto place-self-center p-10 2xl:py-36">
+      <div className="mx-auto items-center justify-items-center place-self-center">
+
         <ProductDetails
           key={sneakerData.styleID}
           styleID={sneakerData.styleID}
@@ -27,6 +29,7 @@ export default async function SneakerPage({ params }: SneakerPageProps) {
           releaseDate={sneakerData.releaseDate}
           colorway={sneakerData.colorway}
         />
+      </div>
       </div>
     </div>
   );
