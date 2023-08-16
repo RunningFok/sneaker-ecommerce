94bec2cb8c8a1192c20d2caab4a3a88187612972
runningfok <adrianfok26@gmail.com>
2023-08-16 10:34:54 +0800

    fixed loading bar spinning

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div
-      className="pt-20 after:animate-spin duration-500 inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-green-600 rounded-full"
+      className="animate-spin duration-500 inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-green-600 rounded-full"
       role="status"
       aria-label="loading"
     >
